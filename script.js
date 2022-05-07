ee7e99f065cc81a2a9ee719768e6fe74cb656fe0
marilymaril <devdaron@gmail.com>
2022-05-07 05:28:49 +0000

    Edit the javascript and some minor fixes

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerText = 
+        "If you deposit " + principal + "\n" +
+        "at an interest rate of " + rate + "%.\n" +
+        "You will receive an amount of " + interest + "\n" +
+        "in the year " + year;
+}
+
+function updateRate() 
+{
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateVal;
 }
         
+function validatePrincipal()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal < 0)
+    {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+    }
+}
